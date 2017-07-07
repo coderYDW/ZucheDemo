a2aa844c92d23eb19b04314af4a56b054ca0c409
DovYoung <1591759055@qq.com>
2017-07-07 14:58:53 +0800

    公交车路线查寻

--- ZucheDemo/ZucheDemo/DYRouteViewController.m
+++ ZucheDemo/ZucheDemo/DYRouteViewController.m
@@ -11,8 +11,11 @@
 #import "CommonUtility.h"
 //114.124223,22.535561  深圳站
 //114.036937,22.616579  深圳北站
+//114.110985,22.567428  红岭北
 #define WZKGDA CLLocationCoordinate2DMake(22.563855, 114.11351)
 #define SZBZ   CLLocationCoordinate2DMake(22.616579, 114.036937)
+#define SZZ   CLLocationCoordinate2DMake(22.535561, 114.124223)
+#define HLBZ   CLLocationCoordinate2DMake(22.567428, 114.110985)
 
 @interface DYRouteViewController () <MAMapViewDelegate,AMapSearchDelegate>
 
@@ -32,6 +35,8 @@
 @property (nonatomic, strong) AMapRoute *route;
 
 @property (nonatomic, strong) MANaviRoute *naviRoute;
+
+@property (nonatomic, assign) BOOL lineOn;
 
 @end
 
@@ -76,20 +81,60 @@
     [self.mapView addAnnotation:destinationAnnotation];
 }
 
-- (void)searchRoute {
-    
+#pragma mark - 搜索各种路线
+
+- (void)searchBusRoute {
+
     self.startAnnotation.coordinate = self.startCoordinate;
     self.destinationAnnotation.coordinate = self.destinationCoordinate;
+    
+    AMapTransitRouteSearchRequest *request = [AMapTransitRouteSearchRequest new];
+    
+    request.origin = [AMapGeoPoint locationWithLatitude:self.startCoordinate.latitude longitude:self.startCoordinate.longitude];
+    request.destination = [AMapGeoPoint locationWithLatitude:self.destinationCoordinate.latitude longitude:self.destinationCoordinate.longitude];
+    
+    request.city = @"深圳";
+    request.requireExtension = YES;
+    
+    [self.search AMapTransitRouteSearch:request];
+    
+}
+
+- (void)searchWalkingRoute {
+
+    self.startAnnotation.coordinate = self.startCoordinate;
+    self.destinationAnnotation.coordinate = self.destinationCoordinate;
+    
+    AMapWalkingRouteSearchRequest *request = [AMapWalkingRouteSearchRequest new];
+    request.origin = [AMapGeoPoint locationWithLatitude:self.startCoordinate.latitude longitude:self.startCoordinate.longitude];
+    request.destination = [AMapGeoPoint locationWithLatitude:self.destinationCoordinate.latitude longitude:self.destinationCoordinate.longitude];
+    request.multipath = 1;
+    [self.search AMapWalkingRouteSearch:request];
+
+}
+
+- (void)searchDriveRoute {
     
     AMapDrivingRouteSearchRequest *request = [[AMapDrivingRouteSearchRequest alloc] init];
     
     request.requireExtension = YES;
-    request.strategy = 0;
+    request.strategy = 5;
     request.origin = [AMapGeoPoint locationWithLatitude:self.startCoordinate.latitude longitude:self.startCoordinate.longitude];
     request.destination = [AMapGeoPoint locationWithLatitude:self.destinationCoordinate.latitude longitude:self.destinationCoordinate.longitude];
     
     [self.search AMapDrivingRouteSearch:request];
 
+}
+
+- (AMapRouteSearchBaseRequest *)originAndDestinationWithRequest:(AMapRouteSearchBaseRequest *)request {
+    
+    self.startAnnotation.coordinate = self.startCoordinate;
+    self.destinationAnnotation.coordinate = self.destinationCoordinate;
+    
+    request.origin = [AMapGeoPoint locationWithLatitude:self.startCoordinate.latitude longitude:self.startCoordinate.longitude];
+    request.destination = [AMapGeoPoint locationWithLatitude:self.destinationCoordinate.latitude longitude:self.destinationCoordinate.longitude];
+    
+    return request;
 }
 
 - (void)AMapSearchRequest:(id)request didFailWithError:(NSError *)error {
@@ -102,18 +147,40 @@
         return;
     }
     
-    NSLog(@"路线个数 = %zd",response.count);
-    
+    NSLog(@"route path count = %zd",response.route.paths.count);
+    NSLog(@"route transit count = %zd",response.route.transits.count);
     self.route = response.route;
     
     if (response.count > 0) {
         //显示路线
-        [self showRoute:request path:response.route.paths.firstObject];
         //[self showAllRoute:request];
+        if ([request isKindOfClass:[AMapTransitRouteSearchRequest class]]) {
+            
+            [self showBusRoute:(AMapTransitRouteSearchRequest *)request transit:response.route.transits.firstObject];
+            
+        }
+        else {
         
+            [self showRoute:request path:response.route.paths.firstObject];
+        
+        }
     }
     
     
+
+}
+
+#pragma mark - 展示路线
+- (void)showBusRoute:(AMapTransitRouteSearchRequest *)request transit:(AMapTransit *)transit {
+
+    self.naviRoute = [MANaviRoute naviRouteForTransit:transit startPoint:request.origin endPoint:request.destination];
+    
+    [self.naviRoute addToMapView:self.mapView];
+    
+    if (self.naviRoute.routePolylines.count) {
+        
+        [self.mapView setVisibleMapRect:[CommonUtility mapRectForOverlays:self.naviRoute.routePolylines] edgePadding:UIEdgeInsetsMake(-10, 10, -10, 10) animated:YES];
+    }
 
 }
 
@@ -127,15 +194,33 @@
 
 - (void)showRoute:(AMapRouteSearchBaseRequest *)request path:(AMapPath *)path {
     
-    self.naviRoute = [MANaviRoute naviRouteForPath:path withNaviType:MANaviAnnotationTypeDrive showTraffic:NO startPoint:request.origin endPoint:request.destination];
+    MANaviAnnotationType type = MANaviAnnotationTypeWalking;
+    if ([request isKindOfClass:[AMapWalkingRouteSearchRequest class]]) {
+        type = MANaviAnnotationTypeWalking;
+    }
+    if ([request isKindOfClass:[AMapDrivingRouteSearchRequest class]]) {
+        type = MANaviAnnotationTypeDrive;
+    }
+    if ([request isKindOfClass:[AMapTransitRouteSearchRequest class]]) {
+        type = MANaviAnnotationTypeBus;
+    }
+    if([request isKindOfClass:[AMapRidingRouteSearchRequest class]]) {
+        type = MANaviAnnotationTypeRiding;
+    }
+    
+    self.naviRoute = [MANaviRoute naviRouteForPath:path withNaviType:type showTraffic:NO startPoint:request.origin endPoint:request.destination];
     
     [self.naviRoute addToMapView:self.mapView];
     
-    [self.mapView setVisibleMapRect:[CommonUtility mapRectForOverlays:self.naviRoute.routePolylines] edgePadding:UIEdgeInsetsMake(-10, 10, -10, 10) animated:YES];
+    if (self.naviRoute.routePolylines.count) {
+        
+        [self.mapView setVisibleMapRect:[CommonUtility mapRectForOverlays:self.naviRoute.routePolylines] edgePadding:UIEdgeInsetsMake(-10, 10, -10, 10) animated:YES];
+    }
+    
 
 }
 
-
+#pragma mark - 线的样式
 - (MAOverlayRenderer *)mapView:(MAMapView *)mapView rendererForOverlay:(id<MAOverlay>)overlay
 {
     if ([overlay isKindOfClass:[LineDashPolyline class]])
@@ -183,6 +268,7 @@
     return nil;
 }
 
+#pragma mark - 大头针样式
 - (MAAnnotationView *)mapView:(MAMapView *)mapView viewForAnnotation:(id<MAAnnotation>)annotation
 {
     if ([annotation isKindOfClass:[MAPointAnnotation class]])
@@ -244,27 +330,33 @@
     return nil;
 }
 
+#pragma mark - 更新位置
 - (void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation updatingLocation:(BOOL)updatingLocation {
-
-    //NSLog(@"%s", __FUNCTION__);
     
     NSLog(@"{%f,%f}",mapView.userLocation.coordinate.latitude,mapView.userLocation.coordinate.longitude);
 
 }
 
+#pragma mark - 地图加载完毕
 - (void)mapViewDidFinishLoadingMap:(MAMapView *)mapView {
     
     NSLog(@"%s", __FUNCTION__);
     
-    self.startCoordinate = self.mapView.userLocation.coordinate;
-    self.destinationCoordinate = SZBZ;
+    if (self.lineOn == NO) {
+        self.lineOn = YES;
+        self.startCoordinate = self.mapView.userLocation.coordinate;
+        self.destinationCoordinate = SZBZ;
+        
+        self.search = [[AMapSearchAPI alloc] init];
+        self.search.delegate = self;
+        
+        [self addDefaultAnnotations];
+        
+        [self searchBusRoute];
+        //[self searchDriveRoute];
+        //[self searchWalkingRoute];
+    }
     
-    self.search = [[AMapSearchAPI alloc] init];
-    self.search.delegate = self;
-    
-    [self addDefaultAnnotations];
-    
-    [self searchRoute];
     
     //[CLLocationManager locationServicesEnabled] && [CLLocationManager authorizationStatus]
 }
